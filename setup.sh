c6f81458c1293f44b8e260851d3dd552798c0f84
Zayxc <141384673+Zayxc@users.noreply.github.com>
2024-10-16 03:46:13 +0700

    setup.sh

--- setup.sh
+++ setup.sh
@@ -4,12 +4,5 @@ dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Dat
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 
-
-"
-echo -ne "[ ${yell}WARNING${NC} ] reboot now ? (y/n)? "
-read answer
-if [ "$answer" == "${answer#[Yy]}" ] ;then
-exit 0
-else
 reboot
 fi
