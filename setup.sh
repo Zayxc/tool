10c43adfe9bc28cd19de018aaba3beaee5ae7ef7
Zayxc <141384673+Zayxc@users.noreply.github.com>
2024-10-15 10:27:02 +0700

    setup.sh

--- setup.sh
+++ setup.sh
@@ -5,11 +5,6 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 
 
-"
-echo -ne "[ ${yell}WARNING${NC} ] reboot now ? (y/n)? "
-read answer
-if [ "$answer" == "${answer#[Yy]}" ] ;then
-exit 0
-else
+
 reboot
 fi
