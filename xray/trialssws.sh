3a0c45dbc8a13369bde11db27893c80a710c0577
Zayxc <141384673+Zayxc@users.noreply.github.com>
2024-11-15 22:17:45 +0700

    trialssws.sh

--- xray/trialssws.sh
+++ xray/trialssws.sh
@@ -17,7 +17,7 @@ shadowsocks_base64e=$(cat /tmp/log1)
 shadowsockslink="ss://${shadowsocks_base64e}@isi_bug_disini:$tls?path=ss-ws&security=tls&host=${domain}&type=ws&sni=${domain}#${user}"
 shadowsockslink2="ss://${shadowsocks_base64e}@isi_bug_disini:$ntls?path=ss-ws&security=tls&host=${domain}&type=ws#${user}"
 shadowsockslink1="ss://${shadowsocks_base64e}@${domain}:$tls?mode=gun&security=tls&type=grpc&serviceName=ss-grpc&sni=bug.com#${user}"
-systemctl restart xray > /dev/null 2>&1
+
 service cron restart > /dev/null 2>&1
 clear
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
