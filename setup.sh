af5d12b8d703f9b64aa511e3aeeeba5469b333d2
Zayxc <141384673+Zayxc@users.noreply.github.com>
2024-11-15 08:10:09 +0700

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,1 @@
 
-#!/bin/bash
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-#########################
-
