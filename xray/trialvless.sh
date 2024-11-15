9d41f82931ed32ed983bfc2e07870392cec506a4
Zayxc <141384673+Zayxc@users.noreply.github.com>
2024-11-15 22:18:29 +0700

    trialvless.sh

--- xray/trialvless.sh
+++ xray/trialvless.sh
@@ -12,7 +12,7 @@ sed -i '/#vlessgrpc$/a\#& '"$user $exp"'\
 vlesslink1="vless://${uuid}@${domain}:$tls?path=/vless&security=tls&encryption=none&type=ws#${user}"
 vlesslink2="vless://${uuid}@${domain}:$none?path=/vless&encryption=none&type=ws#${user}"
 vlesslink3="vless://${uuid}@${domain}:$tls?mode=gun&security=tls&encryption=none&type=grpc&serviceName=vless-grpc&sni=bug.com#${user}"
-systemctl restart xray
+
 clear
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\E[44;1;39m        Trial Vless        \E[0m"
