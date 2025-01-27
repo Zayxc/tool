2f9ff189a2ea87ab45c985cdfce1f7b079f6fb63
Zayxc <141384673+Zayxc@users.noreply.github.com>
2025-01-28 00:23:51 +0700

    cek.sh

--- ssh/cek.sh
+++ ssh/cek.sh
@@ -68,6 +68,5 @@ rm -f /tmp/login-db-pid.txt
 rm -f /tmp/login-db.txt
 rm -f /tmp/vpn-login-tcp.txt
 rm -f /tmp/vpn-login-udp.txt
-read -n 1 -s -r -p "Press any key to back on menu"
-
+read -p "Kembali ke Menu tekan Enter"
 menu
