9630606b5a19595c341975b4791000010f33c44d
Zayxc <141384673+Zayxc@users.noreply.github.com>
2025-01-28 00:29:01 +0700

    trial.sh

--- ssh/trial.sh
+++ ssh/trial.sh
@@ -90,5 +90,5 @@ echo -e "GET / HTTP/1.1[crlf]Host: $domen[crlf]Upgrade: websocket[crlf][crlf]"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 fi
 echo ""
-read -n 1 -s -r -p "Press any key to back on menu"
+read -p "Kembali ke Menu tekan Enter"
 menu
