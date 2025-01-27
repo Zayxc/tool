ad67d0542b30a84beb92be776d370eb4237cdbb8
Zayxc <141384673+Zayxc@users.noreply.github.com>
2025-01-28 01:23:38 +0700

    renew-tr.sh

--- xray/renew-tr.sh
+++ xray/renew-tr.sh
@@ -47,6 +47,6 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#! " "/etc/xray/config.json")
     echo ""
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo ""
-    read -n 1 -s -r -p "Press any key to back on menu"
+    read -p "Kembali ke Menu tekan Enter"
     menu
     fi
