499ad7c22fe9604d273e8fce454b582bf92dc8cd
Zayxc <141384673+Zayxc@users.noreply.github.com>
2025-01-28 01:32:53 +0700

    trialssws.sh

--- xray/trialssws.sh
+++ xray/trialssws.sh
@@ -44,5 +44,6 @@ echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━�
 echo -e "Expired On     : $exp"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo "" | tee -a /etc/log-create-user.log
-read -n 1 -s -r -p "Press any key to back on menu"
+echo ""
+read -p "Kembali ke Menu tekan Enter"
 menu
