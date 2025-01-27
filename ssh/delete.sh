6b7e3cea0deaeba572be21498600477b6a97f144
Zayxc <141384673+Zayxc@users.noreply.github.com>
2025-01-28 00:25:05 +0700

    delete.sh

--- ssh/delete.sh
+++ ssh/delete.sh
@@ -44,6 +44,6 @@ clear
                echo " "
                echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"  
                
-               read -n 1 -s -r -p "Press any key to back on menu"
+               read -p "Kembali ke Menu tekan Enter"
                menu
         
