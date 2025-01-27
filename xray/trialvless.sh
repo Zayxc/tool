eecfe718c8fa09ec1388bfd2fb718e27d9fe81fc
Zayxc <141384673+Zayxc@users.noreply.github.com>
2025-01-28 01:34:33 +0700

    trialvless.sh

--- xray/trialvless.sh
+++ xray/trialvless.sh
@@ -38,5 +38,5 @@ echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━�
 echo -e "Expired On     : $exp"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
-read -n 1 -s -r -p "Press any key to back on menu"
+read -p "Kembali ke Menu tekan Enter"
 menu
