b84576ef56aac5e2342acfdc8719b64a8e1565bc
Zayxc <141384673+Zayxc@users.noreply.github.com>
2025-10-02 11:31:50 +0700

    insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -12,10 +12,10 @@ chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-stunnel
 
 #System Dropbear Websocket-SSH Python
-wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/Zayxc/tool/aio/sshws/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
+wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/Zayxc/tool/refs/heads/aio/sshws/ws-dropbear.service && chmod +x /etc/systemd/system/ws-dropbear.service
 
 #System SSL/TLS Websocket-SSH Python
-wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/Zayxc/tool/aio/sshws/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
+wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/Zayxc/tool/refs/heads/aio/sshws/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
 
 
 #restart service
